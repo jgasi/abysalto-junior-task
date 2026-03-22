80dcbde9cf776bc77f46408ab3fd07137e5cc9ae
juraj <juraj@DESKTOP-QAJPGCA>
2026-03-22 16:32:33 +0100

    Added test project and installed dependencies.

---
+++ AbySalto.Junior.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AbySalto.Junior.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
